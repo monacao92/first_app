b59717cfc4fc487cc572a05086ed9e1055b899d5
Mona Cao <mona.cao@yale.edu>
2012-07-23 15:30:39 -0400

    Git push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1f3c3c91f757ec5a2f76b78309d4a36d5e5c7bf4da84663168a2bbff499940f090b4102926c3c0dcd6b8d3eadb0e21b97e3684c3fd2ca9c0143671864f747d6f'
